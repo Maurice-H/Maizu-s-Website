d3dc8a5ad851db52e4cd515a46b77813b82a62ae
Maurice-H <xmaui2002x@yahoo.de>
2024-02-10 23:06:43 +0100

    Finished GamesPage

--- src/Views/GamesPage.tsx
+++ src/Views/GamesPage.tsx
@@ -1,9 +1,15 @@
+import Footer from "../Sections/Footer";
+import GameHistory from "../Sections/GameHistory";
 import Header from "../Sections/Header.tsx";
+import UnityGame from "../Sections/UnityGame";
 
 function GamesPage() {
   return (
     <>
       <Header />
+      <UnityGame />
+      <GameHistory />
+      <Footer />
     </>
   );
 }
